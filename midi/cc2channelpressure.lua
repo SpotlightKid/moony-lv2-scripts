443573f256e27597539daecddb714ae38f9b95b6
Christopher Arndt <chris@chrisarndt.de>
2020-09-08 00:29:31 +0200

    Define proper parameters for cc2channelpressure and channelpressure2cc
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- midi/cc2channelpressure.lua
+++ midi/cc2channelpressure.lua
@@ -1,32 +1,85 @@
 -- Convert specific Control Change events to MIDI Channel Pressure (aka Mono Aftertouch)
 
+-- version 0.2.0
+
+-- define URI prefix for state parameters
+local urn = Mapper('urn:uuid:278c7f88-f157-11ea-ad34-3c970e9a9ec9#')
+
 -- 0..15 or set to -1 to apply to events on all channels
-local filter_chan = -1
+local filter_chan = Parameter {
+  [RDFS.label] = 'Filter channel',
+  [RDFS.comment] = 'Set MIDI channel to which this filter is applied. Set to -1 for all channels',
+  [RDFS.range] = Atom.Int,
+  [LV2.minimum] = -1,
+  [LV2.maximum] = 15,
+  [RDF.value] = -1
+}
 -- which control change number to convert?
-local source_cc = 1
--- whether to pass non-CC and other CC events
-local pass_other = true
+local source_cc = Parameter {
+  [RDFS.label] = 'Source CC',
+  [RDFS.comment] = 'Set control change number to convert from',
+  [RDFS.range] = Atom.Int,
+  [LV2.minimum] = 0,
+  [LV2.maximum] = 127,
+  [RDF.value] = 1
+}
+-- whether to pass unmatched CC events
+local pass_unmatched = Parameter {
+  [RDFS.label] = 'Pass unmatched events?',
+  [RDFS.comment] = 'Should non-matching CC events be passed through?',
+  [RDFS.range] = Atom.Bool,
+  [RDF.value] = true
+}
 
 
--- NO NEED TO CHANGE ANYTHING BELOW
+-- define a StateResponder object
+local stateR = StateResponder({
+  [Patch.writable] = {
+    [urn.filter_chan] = filter_chan,
+    [urn.source_cc] = source_cc,
+    [urn.pass_unmatched] = pass_unmatched,
+  }
+})
+
+-- push parameter values to disk
+function save(forge)
+  stateR:stash(forge)
+end
+
+-- pop parameter values from disk
+function restore(atom)
+  stateR:apply(atom)
+end
+
+-- register parameters to UI
+function once(n, control, notify)
+  stateR:register(0, notify)
+end
+
 
 function cc2channelpressure(self, frames, forge, chan, cc, value)
-  if filter_chan == -1 or chan == filter_chan then
-    if cc == source_cc then
+  if filter_chan() == -1 or chan == filter_chan() then
+    if cc == source_cc() then
         forge:time(frames):midi(MIDI.ChannelPressure | chan, value)
-    elseif pass_other then
+    elseif pass_unmatched() then
         forge:time(frames):midi(MIDI.Controller | chan, cc, value)
     end
+  elseif pass_unmatched() then
+    forge:time(frames):midi(MIDI.Controller | chan, cc, value)
   end
 end
 
 -- define a MIDIResponder object to handle control change events
 local midiR = MIDIResponder({
   [MIDI.Controller] = cc2channelpressure,
-}, pass_other)
+}, true)
 
 function run(n, control, notify, seq, forge)
-  -- iterate over incoming events
+  -- iterate over incoming control events
+  for frames, atom in control:foreach() do
+    local handled = stateR(frames, notify, atom)
+  end
+  -- iterate over incoming sequence events
   for frames, atom in seq:foreach() do
     -- call responder for event
     local handled = midiR(frames, forge, atom)
--- midi/channelpressure2cc.lua
+++ midi/channelpressure2cc.lua
@@ -1,28 +1,81 @@
 -- Convert MIDI Channel Pressure (aka Mono Aftertouch) to Control Change events
 
+-- version 0.2.0
+
+-- define URI prefix for state parameters
+local urn = Mapper('urn:uuid:a21213a2-f14e-11ea-ad18-3c970e9a9ec9#')
+
 -- 0..15 or set to -1 to apply to events on all channels
-local filter_chan = -1
+local filter_chan = Parameter {
+  [RDFS.label] = 'Filter channel',
+  [RDFS.comment] = 'Set MIDI channel to which this filter is applied. Set to -1 for all channels',
+  [RDFS.range] = Atom.Int,
+  [LV2.minimum] = -1,
+  [LV2.maximum] = 15,
+  [RDF.value] = -1
+}
 -- which control change number to convert to?
-local dest_cc = 1
--- whether to pass non-mono AT events
-local pass_other = true
+local dest_cc = Parameter {
+  [RDFS.label] = 'Destination CC',
+  [RDFS.comment] = 'Set control change number to convert to',
+  [RDFS.range] = Atom.Int,
+  [LV2.minimum] = 0,
+  [LV2.maximum] = 127,
+  [RDF.value] = 1
+}
+-- whether to pass unmatched mono AT events
+local pass_unmatched = Parameter {
+  [RDFS.label] = 'Pass unmatched events?',
+  [RDFS.comment] = 'Should channel pressure events not on filter channel be passed through?',
+  [RDFS.range] = Atom.Bool,
+  [RDF.value] = true
+}
 
 
--- NO NEED TO CHANGE ANYTHING BELOW
+-- define a StateResponder object
+local stateR = StateResponder({
+  [Patch.writable] = {
+    [urn.filter_chan] = filter_chan,
+    [urn.dest_cc] = dest_cc,
+    [urn.pass_unmatched] = pass_unmatched,
+  }
+})
+
+-- push parameter values to disk
+function save(forge)
+  stateR:stash(forge)
+end
+
+-- pop parameter values from disk
+function restore(atom)
+  stateR:apply(atom)
+end
+
+-- register parameters to UI
+function once(n, control, notify)
+  stateR:register(0, notify)
+end
+
 
 function channelpressure2cc(self, frames, forge, chan, value)
-  if filter_chan == -1 or chan == filter_chan then
-    forge:time(frames):midi(MIDI.Controller | chan, dest_cc, value)
+  if filter_chan() == -1 or chan == filter_chan() then
+    forge:time(frames):midi(MIDI.Controller | chan, dest_cc(), value)
+  elseif pass_unmatched() then
+    forge:time(frames):midi(MIDI.ChannelPressure | chan, value)
   end
 end
 
 -- define a MIDIResponder object to handle channel pressure events
 local midiR = MIDIResponder({
   [MIDI.ChannelPressure] = channelpressure2cc,
-}, pass_other)
+}, true)
 
 function run(n, control, notify, seq, forge)
-  -- iterate over incoming events
+  -- iterate over incoming control events
+  for frames, atom in control:foreach() do
+    local handled = stateR(frames, notify, atom)
+  end
+  -- iterate over incoming sequence events
   for frames, atom in seq:foreach() do
     -- call responder for event
     local handled = midiR(frames, forge, atom)
