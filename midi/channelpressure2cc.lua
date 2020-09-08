d8fe263d469459226db48eafe9dcd769599b7ce3
Christopher Arndt <chris@chrisarndt.de>
2020-09-08 23:10:53 +0200

    Improve parameter handling and fix pass_unmatched param
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- midi/channelpressure2cc.lua
+++ midi/channelpressure2cc.lua
@@ -1,18 +1,40 @@
 -- Convert MIDI Channel Pressure (aka Mono Aftertouch) to Control Change events
 
--- version 0.2.0
+local version = "0.2.0"
+print("Loading channelpressure2cc filter version " .. version .. " ...")
 
 -- define URI prefix for state parameters
 local urn = Mapper('urn:uuid:a21213a2-f14e-11ea-ad18-3c970e9a9ec9#')
+local midiR
 
+-- define parameters
 -- 0..15 or set to -1 to apply to events on all channels
 local filter_chan = Parameter {
   [RDFS.label] = 'Filter channel',
-  [RDFS.comment] = 'Set MIDI channel to which this filter is applied. Set to -1 for all channels',
+  [RDFS.comment] = 'Set MIDI channel to which this filter is applied.',
   [RDFS.range] = Atom.Int,
+  [RDF.value] = -1,
   [LV2.minimum] = -1,
   [LV2.maximum] = 15,
-  [RDF.value] = -1
+  [LV2.scalePoint] = {
+    ["All Channels"] = -1,
+    ["Channel 1"] = 0,
+    ["Channel 2"] = 1,
+    ["Channel 3"] = 2,
+    ["Channel 4"] = 3,
+    ["Channel 5"] = 4,
+    ["Channel 6"] = 5,
+    ["Channel 7"] = 6,
+    ["Channel 8"] = 7,
+    ["Channel 9"] = 8,
+    ["Channel 10"] = 9,
+    ["Channel 11"] = 10,
+    ["Channel 12"] = 11,
+    ["Channel 13"] = 12,
+    ["Channel 14"] = 13,
+    ["Channel 15"] = 14,
+    ["Channel 16"] = 15,
+  }
 }
 -- which control change number to convert to?
 local dest_cc = Parameter {
@@ -25,10 +47,17 @@ local dest_cc = Parameter {
 }
 -- whether to pass unmatched mono AT events
 local pass_unmatched = Parameter {
-  [RDFS.label] = 'Pass unmatched events?',
-  [RDFS.comment] = 'Should channel pressure events not on filter channel be passed through?',
+  [RDFS.label] = 'Pass umatched?',
+  [RDFS.comment] = 'Should unvonverted events be passed through?',
   [RDFS.range] = Atom.Bool,
-  [RDF.value] = true
+  _value = true,
+  [Patch.Get] = function (self)
+    return self._value
+  end,
+  [Patch.Set] = function (self, value)
+    self._value = value
+    midiR.through = value
+  end
 }
 
 
@@ -57,6 +86,7 @@ function once(n, control, notify)
 end
 
 
+-- convert  channel pressure events if MIDI channel matches
 function channelpressure2cc(self, frames, forge, chan, value)
   if filter_chan() == -1 or chan == filter_chan() then
     forge:time(frames):midi(MIDI.Controller | chan, dest_cc(), value)
@@ -65,19 +95,23 @@ function channelpressure2cc(self, frames, forge, chan, value)
   end
 end
 
--- define a MIDIResponder object to handle channel pressure events
-local midiR = MIDIResponder({
-  [MIDI.ChannelPressure] = channelpressure2cc,
-}, true)
 
+-- define a MIDIResponder object to handle channel pressure events
+midiR = MIDIResponder({
+  [MIDI.ChannelPressure] = channelpressure2cc,
+}, pass_unmatched())
+
+
+-- the main processing function
 function run(n, control, notify, seq, forge)
   -- iterate over incoming control events
   for frames, atom in control:foreach() do
-    local handled = stateR(frames, notify, atom)
+    stateR(frames, notify, atom)
   end
   -- iterate over incoming sequence events
   for frames, atom in seq:foreach() do
-    -- call responder for event
-    local handled = midiR(frames, forge, atom)
+    midiR(frames, forge, atom)
   end
 end
+
+print("Loading channelpressure2cc complete.")
